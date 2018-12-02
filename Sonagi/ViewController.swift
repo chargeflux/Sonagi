eb06def8f798acad762c49efd7d78e3832010e47
chargeflux <20979457+chargeflux@users.noreply.github.com>
2018-12-02 18:54:52 -0500

    Initial Commit

---
+++ Sonagi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sonagi
+//
+//  Created by chargeflux on 12/2/18.
+//  Copyright © 2018 chargeflux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
