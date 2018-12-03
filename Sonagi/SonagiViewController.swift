744d8667b143eba98806194af89ffc451b0ecb77
chargeflux <20979457+chargeflux@users.noreply.github.com>
2018-12-02 23:09:38 -0500

    Added func to manipulate text in outputTextView

--- Sonagi/SonagiViewController.swift
+++ Sonagi/SonagiViewController.swift
@@ -10,8 +10,11 @@ import Cocoa
 
 class SonagiViewController: NSViewController {
     
+    @IBOutlet var outputTextView: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setText()
     }
 
     override var representedObject: Any? {
@@ -19,7 +22,14 @@ class SonagiViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    func setText() {
+        let font = NSFont(name: "NanumSquareR", size: 32) ?? NSFont.systemFont(ofSize: 32)
+        let attributes: [NSAttributedString.Key : Any] = [NSAttributedString.Key.font:font,
+                          NSAttributedString.Key.foregroundColor:NSColor.white]
+        let text = NSAttributedString(string: "저는 내년에 한국에 갈 거예요",attributes:attributes)
+        self.outputTextView.textStorage?.append(text)
+    }
 
 }
 
