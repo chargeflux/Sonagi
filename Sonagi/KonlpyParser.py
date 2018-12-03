2a213eadb751ddf0c8a1a9b6df5b1ab1b287e0e3
chargeflux <20979457+chargeflux@users.noreply.github.com>
2018-12-02 20:35:51 -0500

    Implemented PartOfSpeech class in SonagiParser and added --type argument to py file

--- Sonagi/KonlpyParser.py
+++ Sonagi/KonlpyParser.py
@@ -1,3 +1,11 @@
+#
+#  KonlpyParser.py
+#  Sonagi
+#
+#  Created by chargeflux on 12/2/18.
+#  Copyright © 2018 chargeflux. All rights reserved.
+#
+
 from konlpy.tag import Okt
 import sys
 import unicodedata
@@ -6,6 +14,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Find POS for input (Korean)')
 parser.add_argument('--input', type=str, help='Input string for parsing')
+parser.add_argument('--type', type=str, help='Specify what to extract')
 args = parser.parse_args()
 
 input = args.input
@@ -14,6 +23,6 @@ input = unicodedata.normalize('NFC', input) #NFD doesn't work
 
 okt = Okt()
 
-pos = okt.pos(input,norm=True, stem=True,join=True)
-
-sys.stdout.buffer.write('\n'.join(pos).encode('utf-8'))
+if args.type == "pos":
+    pos = okt.pos(input,norm=True, stem=True,join=True)
+    sys.stdout.buffer.write('\n'.join(pos).encode('utf-8'))
--- Sonagi/SonagiParser.swift
+++ Sonagi/SonagiParser.swift
@@ -7,8 +7,31 @@
 //
 
 import Cocoa
+import Foundation
 
-class SonagiParser {
+
+class PartOfSpeech {
+    var inputKR: String!
+    var task = Process()
+    var pipe = Pipe()
+    var posDict: [Int:(morph:String,pos:String)] = [:]
     
-
+    func parseInputForPOS() {
+        task.executableURL = URL(fileURLWithPath: "/usr/local/bin/python3")
+        let filePath = Bundle.main.url(forResource: "KonlpyParser", withExtension: "py")
+        task.arguments = [filePath!.absoluteString,"--type", "pos","--input",inputKR]
+        task.standardOutput = pipe
+        try? task.run()
+        
+        let handle = pipe.fileHandleForReading
+        let data = handle.readDataToEndOfFile()
+        let out = String(data: data, encoding: String.Encoding.utf8)
+        
+        let outArray = out?.components(separatedBy: "\n")
+        
+        for (index, entry) in outArray!.enumerated() {
+            var tempArr = entry.components(separatedBy: "/")
+            posDict.updateValue((morph: tempArr[0], pos: tempArr[1]), forKey: index)
+        }
+    }
 }
