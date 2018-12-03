d09ac08bda90655922995acc4c9b97a74517470a
chargeflux <20979457+chargeflux@users.noreply.github.com>
2018-12-02 22:07:47 -0500

    Initialized main window with ultra dark BG

--- Sonagi/SonagiViewController.swift
+++ Sonagi/SonagiViewController.swift
@@ -8,12 +8,10 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class SonagiViewController: NSViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
