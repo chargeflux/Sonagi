ab5bffb4a2666a13c6aff823a7c47ab46b844350
chargeflux <20979457+chargeflux@users.noreply.github.com>
2018-12-04 23:31:47 -0500

    Implemented spacing logic in outputTextView text

--- Sonagi/SonagiViewController.swift
+++ Sonagi/SonagiViewController.swift
@@ -40,13 +40,47 @@ class SonagiViewController: NSViewController {
     
     func setText(input: PartOfSpeech) {
         let font = NSFont(name: "NanumSquareR", size: 32) ?? NSFont.systemFont(ofSize: 32)
+        var textKRFullString = textKR!
         for key in input.posDict.keys.sorted() {
             let attributes: [NSAttributedString.Key : Any] = [NSAttributedString.Key.font:font,
                                                               NSAttributedString.Key.foregroundColor:
                                                                 input.posDict[key]!.color!]
             text = NSAttributedString(string: input.posDict[key]!.morph,attributes:attributes)
-            outputTextView.textStorage?.append(text!)
+            let textCommon = textKRFullString.commonPrefix(with: (text?.string)!)
+            var isWhiteSpace: Bool!
+            if textCommon.count == text?.string.count {
+                (isWhiteSpace, textKRFullString) = checkWhiteSpace(fullString: textKRFullString, commonString: textCommon, textToSet: text!)
+                if isWhiteSpace {
+                    outputTextView.textStorage?.append(text!)
+                    outputTextView.textStorage?.append(NSAttributedString(string:" "))
+                }
+                else {
+                    outputTextView.textStorage?.append(text!)
+                }
+            }
+            else {
+                (isWhiteSpace, textKRFullString) = checkWhiteSpace(fullString: textKRFullString, commonString: textCommon, textToSet: text!)
+                text = NSAttributedString(string:textCommon, attributes:attributes)
+                outputTextView.textStorage?.append(text!)
+                if isWhiteSpace {
+                    outputTextView.textStorage?.append(NSAttributedString(string:" "))
+                }
+                
+            }
         }
+    }
+    
+    func checkWhiteSpace(fullString: String, commonString: String, textToSet: NSAttributedString) -> (Bool?, String) {
+        let lastCommonIndex = fullString.index(fullString.startIndex, offsetBy: commonString.count-1)
+        
+        if let afterLastCommonIndex = fullString.index(lastCommonIndex,offsetBy: 1, limitedBy: fullString.endIndex) as String.Index?, afterLastCommonIndex != fullString.endIndex {
+            // if afterLastCommonIndex == fullString.endIndex, function does not return nil; necessary to check
+                if fullString[afterLastCommonIndex] == " " {
+                    return (true, String(fullString[fullString.index(after:afterLastCommonIndex)...]))
+                }
+            return (false, String(fullString[afterLastCommonIndex...]))
+        }
+        return (false, fullString)
     }
     
     func setTracking() {
